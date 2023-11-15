944794dd35b54e0c14967d9a209604fa3f96300c
Ramboo103 <ramboo3110@gmail.com>
2023-11-15 15:25:55 +0700

    test commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from '../reducers/rootReducer';
-import productSaga from './productSaga'
-import createSagaMiddleware from 'redux-saga';
+// import productSaga from './productSaga'
+// import createSagaMiddleware from 'redux-saga';
 
 // const store = createStore(rootReducer);
 const sagaMiddleware = createSagaMiddleware();
